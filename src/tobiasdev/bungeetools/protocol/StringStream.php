39c037636da9237ebbbbb3823f0f28def94945c4
TobiasDev <tobiasdevde@gmail.com>
2019-10-10 19:02:17 +0200

    Update Stringstream & Remove Debugging Methods

--- src/tobiasdev/bungeetools/protocol/StringStream.php
+++ src/tobiasdev/bungeetools/protocol/StringStream.php
@@ -30,13 +30,13 @@ class StringStream
             // read first two length bytes
             $length = Binary::readShort($this->buffer{$this->pointer} . $this->buffer{$this->pointer + 1});
             // add 2 to pointer
-            $this->changePointer($this->pointer + 2);
+            $this->pointer += 2;
             // read length bytes far and append to string
             $d = "";
             for ($i = $this->pointer; $i < $this->pointer + $length; $i++) {
                 $d .= $this->buffer{$i};
             }
-            $this->changePointer($this->pointer + $length);
+            $this->pointer += $length;
             return $d;
         } catch (Exception $e) {
             MainLogger::getLogger()->warning("Error while decoding string: " . $this->buffer . " ( " . $e->getMessage() . ", LINE: " . $e->getLine() . " )");
@@ -51,22 +51,15 @@ class StringStream
             $offset = $this->pointer;
             return Binary::readInt($this->buffer{$offset} . $this->buffer{$offset + 1} . $this->buffer{$offset + 2} . $this->buffer{$offset + 3});
         } catch (Exception $e) {
-            var_dump("Target Buffer: " . $this->buffer);
             MainLogger::getLogger()->warning("Error while decoding int: " . $this->buffer . " ( " . $e->getMessage() . ", LINE: " . $e->getLine() . " )");
             return null;
         }
     }
 
-    public function changePointer(int $newpointer)
-    {
-        MainLogger::getLogger()->debug("Changed pointer from " . $this->pointer . " to " . $newpointer);
-        $this->pointer = $newpointer;
-    }
     public function readUnsignedShort(): ?int {
         try{
             return Binary::readUnsignedVarInt($this->buffer, $this->pointer);
         }catch (Exception $e) {
-            var_dump("Target Buffer: " . $this->buffer);
             MainLogger::getLogger()->warning("Error while decoding unsignedShort: " . $this->buffer . " ( " . $e->getMessage() . ", LINE: " . $e->getLine() . " )");
             return null;
         }
